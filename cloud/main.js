6d61d621d90d6430fcd9397f050c1f8557f4d7c1
Brad Roush <roush.brad@gmail.com>
2016-03-07 11:38:16 -0800

    Added before save function

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,9 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+Parse.Cloud.beforeSave("Question", function(request, response) {
+	request.object.set("status", "Assigned to Squad 18");
+  response.success();
+});
